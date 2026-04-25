3a36875c58615be7c425e5c64dd099c3113dc3b8
Rohithasiri <y.rohithasiri@gmail.com>
2026-04-25 10:02:32 +0530

    Fix emergency link URL

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import EmergencyView from './pages/EmergencyView';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import AuthPage from './pages/AuthPage';
@@ -8,7 +9,6 @@ import AllergiesPage from './pages/AllergiesPage';
 import MedicinesPage from './pages/MedicinesPage';
 import SurgeriesPage from './pages/SurgeriesPage';
 import UploadReport from './pages/UploadReport';
-import EmergencyView from './pages/EmergencyView';
 import ProfilePage from './pages/ProfilePage';
 import VerifyGoogle from './pages/VerifyGoogle';
 
@@ -25,7 +25,7 @@ function App() {
         <Route path="/medicines" element={<MedicinesPage />} />
         <Route path="/surgeries" element={<SurgeriesPage />} />
         <Route path="/upload" element={<UploadReport />} />
-        <Route path="/emergency/:userId" element={<EmergencyView />} />
+        <Route path="/emergency/:userId" element={<EmergencyView />} /> 
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
